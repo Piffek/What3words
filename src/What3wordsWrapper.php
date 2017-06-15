a1565041e3591399b6b7fc68e653aae16fb94fc0
Piffek <patrykpiwko123412@gmail.com>
2017-06-15 20:30:44 +0200

    add method to client

--- src/What3wordsWrapper.php
+++ src/What3wordsWrapper.php
@@ -5,26 +5,70 @@ use Piffek\What3words\ClientInterface;
 
 class What3wordsWrapper implements ClientInterface
 {
+	protected $key;
 	
-	public function wordsToCoords($words, $language = 'en'){
+	public function __construct(string $key){
 		
+		$this->key = $key;
+		
+	}
+
+	
+	public function wordsToCoords($words, $language = 'en') : array{
+
+		$url = "https://api.what3words.com/v2/forward?addr=%s&key=%s&lang=%s&format=json&display=full";
+
+		$sprintURL = sprintf($url, $words, $this->key, $language);
+		
+		$res = json_decode($this->curlOptions($sprintURL), true);
+
+		return $res['geometry'];
 		
 	}
 	
-	public function coordsToWords($lat, $lng, $language = 'en'){
+	public function coordsToWords($lat, $lng, $language = 'en') : string{
+
+		$url = "https://api.what3words.com/v2/reverse?coords=%s,%s&key=%s&lang=%s&format=json&display=full";
+		
+		$sprintURL = sprintf($url, $lat, $lng, $this->key, $language);
+		
+		$word = json_decode($this->curlOptions($sprintURL), true);
+		
+		return $word['words'];
 		
 	}
 	
-	public function getLanguages(){
+	public function getLanguages() : array{
 		
 	}
 	
-	public function getBlend($words, array $nearby = null){
+	public function getBlend($words, array $nearby = null) : array{
 		
 	}
 	
-	public function autosuggest($words, array $nearby = null, $radius = 10){
+	public function autosuggest($words, array $nearby = null, $radius = 10) : array{
 		
+	}
+	
+	private function curlOptions($sprintURL){
+		$curl = curl_init();
+		
+		curl_setopt_array($curl, array(
+				CURLOPT_URL => $sprintURL,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+		));
+		
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		
+		curl_close($curl);
+		
+		return $response;
 	}
 	
 }
